5358dec4b7642842379e052478824f24ccd61193
Javailabe <kulecki.pawel@gmail.com>
2022-11-04 21:29:37 +0100

    BaseEntity, Author and Book added

---
+++ src/main/java/pl/booky/catalog/domains/Book.java
@@ -0,0 +1,57 @@
+package pl.booky.catalog.domains;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import pl.booky.jpa.BaseEntity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString(exclude = "authors")
+@RequiredArgsConstructor
+@Entity
+public class Book extends BaseEntity {
+
+    @Column(unique = true)
+    private String title;
+
+    private Integer year;
+    private BigDecimal price;
+    private Long coverId;
+    private Long available;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable
+    @JsonIgnoreProperties("books")
+    private Set<Author> authors = new HashSet<>();
+
+    public Book(String title, Integer year, BigDecimal price, Long available) {
+        this.title = title;
+        this.year = year;
+        this.price = price;
+        this.available = available;
+    }
+
+    public void addAuthor(Author author) {
+        authors.add(author);
+        author.getBooks().add(this);
+    }
+
+    public void removeAuthor(Author author) {
+        authors.remove(author);
+        author.getBooks().remove(this);
+    }
+
+    public void removeAuthors() {
+        Book self = this;
+        authors.forEach(author -> author.getBooks().remove(self));
+        authors.clear();
+    }
+}
