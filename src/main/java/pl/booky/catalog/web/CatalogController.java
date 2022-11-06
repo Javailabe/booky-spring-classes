b2525e56e64e0e5805441c081d527a0a45f77b56
Javailabe <kulecki.pawel@gmail.com>
2022-11-06 21:08:10 +0100

    CatalogController updated by additional endpoint and logic

--- src/main/java/pl/booky/catalog/web/CatalogController.java
+++ src/main/java/pl/booky/catalog/web/CatalogController.java
@@ -3,17 +3,24 @@ package pl.booky.catalog.web;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.booky.catalog.application.port.CatalogUseCase;
 import pl.booky.catalog.domains.Author;
 import pl.booky.catalog.domains.Book;
+import pl.booky.web.CreatedURI;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import javax.validation.constraints.*;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -87,6 +94,43 @@ public class CatalogController {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, message);
         }
     }
+
+    @Secured("ROLE_ADMIN")
+    @PutMapping(value = "/{id}/cover", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void addBookCover(@PathVariable Long id, @RequestParam("file")MultipartFile file) throws IOException {
+        catalog.updateBookCover(new UpdateBookCoverCommand(
+                id,
+                file.getBytes(),
+                file.getContentType(),
+                file.getOriginalFilename()
+        ));
+    }
+
+    @Secured("ROLE_ADMIN")
+    @DeleteMapping("/{id}/cover")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeBookCover(@PathVariable Long id) {
+        catalog.removeBookCover(id);
+    }
+    @Secured("ROLE_ADMIN")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Void> addBook(@Valid @RequestBody RestBookCommand command) {
+        Book book = catalog.addBook(command.toCreateCommand());
+        return ResponseEntity.created(createdBookUri(book)).build();
+    }
+    @Secured("ROLE_ADMIN")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) {
+        catalog.removeById(id);
+    }
+
+    private URI createdBookUri(Book book) {
+        return new CreatedURI("/" + book.getId().toString()).uri();
+    }
+
     @Data
     private static class RestBookCommand {
         @NotBlank(message = "Please provide title")
