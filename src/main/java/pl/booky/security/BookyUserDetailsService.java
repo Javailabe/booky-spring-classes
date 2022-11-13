d18a00b36002d43d41a2d71662a10b1016e69fe0
Javailabe <kulecki.pawel@gmail.com>
2022-11-13 12:24:11 +0100

    BookyUserDetailsService implemented

---
+++ src/main/java/pl/booky/security/BookyUserDetailsService.java
@@ -0,0 +1,23 @@
+package pl.booky.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@AllArgsConstructor
+public class BookyUserDetailsService implements UserDetailsService {
+
+    private final UserEntityRepository repository;
+    private final AdminConfig config;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if (config.getUserName().equalsIgnoreCase(username)) {
+            return config.adminUser();
+        }
+        return repository.findByUsernameIgonoreCase(username)
+                .map(UserEntityDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
