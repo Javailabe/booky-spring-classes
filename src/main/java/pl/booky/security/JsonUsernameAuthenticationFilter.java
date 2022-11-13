0244ba33b72dde9d1c72edc49d07b5b5589439a8
Javailabe <kulecki.pawel@gmail.com>
2022-11-13 12:32:22 +0100

    JsonUsernameAuthenticationFilter updated and LoginCommand implemented

--- src/main/java/pl/booky/security/JsonUsernameAuthenticationFilter.java
+++ src/main/java/pl/booky/security/JsonUsernameAuthenticationFilter.java
@@ -1,6 +1,7 @@
 package pl.booky.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -9,10 +10,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class JsonUsernameAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+class JsonUsernameAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final ObjectMapper mapper = new ObjectMapper();
 
+    @SneakyThrows
+    @Override
     public Authentication attemptAuthetication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
         LoginCommand command = mapper.readValue(request.getReader(), LoginCommand.class);
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
