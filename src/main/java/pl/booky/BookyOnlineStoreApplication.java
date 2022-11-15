17437902858346602bdfacd59010324da729686c
Javailabe <kulecki.pawel@gmail.com>
2022-11-15 20:50:25 +0100

    JpaConfiguration implemented, @Bean annotation added to main class

--- src/main/java/pl/booky/BookyOnlineStoreApplication.java
+++ src/main/java/pl/booky/BookyOnlineStoreApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +17,7 @@ public class BookyOnlineStoreApplication {
         SpringApplication.run(BookyOnlineStoreApplication.class, args);
     }
 
+    @Bean
     RestTemplate restTemplate() {
         return new RestTemplateBuilder().build();
     }
